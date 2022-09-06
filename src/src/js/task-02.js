8018f4d776dc977018a3a6d3e80115d8ec8e5413
GannaKov <zlatta2000@gmail.com>
2022-09-07 01:38:17 +0300

    Task 2

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -1,8 +1,27 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const listEl = document.querySelector("#ingredients");
+const elementsList1 = ingredients.map((item) => {
+  const listItemEl1 = document.createElement("li");
+  listItemEl1.textContent = item;
+  listItemEl1.classList.add("item");
+  return listItemEl1;
+});
+listEl.append(...elementsList1);
+console.log(listEl.children);
+//---------------------------------- old style )))
+
+// const elementsList = [];
+// for (let i = 0; i < ingredients.length; i++) {
+//   const listItemEl = document.createElement("li");
+//   listItemEl.textContent = ingredients[i];
+//   listItemEl.classList.add("item");
+//   elementsList.push(listItemEl);
+// }
+// listEl.append(...elementsList);
