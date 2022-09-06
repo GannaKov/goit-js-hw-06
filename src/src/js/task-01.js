b565492f7f3129d5dffeafe6ae37ce4af780ef6a
GannaKov <zlatta2000@gmail.com>
2022-09-06 23:35:18 +0300

    Task 1

---
+++ src/src/js/task-01.js
@@ -0,0 +1,30 @@
+const categoriesEl = document.querySelector("#categories");
+
+const categoriesListEl = categoriesEl.querySelectorAll("ul");
+
+console.log(`Number of categories:${categoriesListEl.length}`);
+
+const categoriesItemTitleArr = categoriesEl.querySelectorAll("h2");
+for (let i = 0; i < categoriesListEl.length; i++) {
+  const categoriesItemTitle = categoriesItemTitleArr[i].textContent;
+
+  const categoriesQuantityEl =
+    categoriesListEl[i].querySelectorAll("li").length;
+  console.log(`Category:${categoriesItemTitle}`);
+  console.log(`Elements:${categoriesQuantityEl}`);
+}
+// ---------------Var2 запасной--------------
+// const categoriesListEl1 = categoriesEl.children; //масс детей главной ul
+
+// console.log(categoriesListEl1);
+// // console.log(`Number of categories:${categoriesListEl1.length}`);
+// for (let i = 0; i < categoriesListEl.length; i++) {
+//   const categoriesItemTitle1 =
+//     categoriesListEl1[i].querySelector("h2").textContent;
+
+//   const categoriesQuantityEl1 =
+//     categoriesListEl1[i].querySelectorAll("li").length;
+//   console.log(`Category:${categoriesItemTitle1}`);
+//   console.log(`Elements:${categoriesQuantityEl1}`);
+// }
+// -------------------
