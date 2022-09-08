14171bb681af0d02c2ec5a3bd95c9815fea37982
GannaKov <zlatta2000@gmail.com>
2022-09-09 02:11:46 +0300

    task 5

---
+++ src/src/js/task-05.js
@@ -0,0 +1,22 @@
+const refs = {
+  inputEl: document.querySelector("#name-input"),
+  spanName: document.querySelector("#name-output"),
+};
+
+refs.inputEl.addEventListener("input", onInputChange);
+
+function onInputChange(event) {
+  refs.spanName.textContent = event.currentTarget.value;
+  if (refs.spanName.textContent === "") {
+    refs.spanName.textContent = "Anonymous";
+  }
+}
+// -------------- 2 Var -----------
+
+// const inputClick = refs.inputEl.addEventListener("input", (event) => {
+//
+//   refs.spanName.textContent = event.currentTarget.value;
+//   if (refs.spanName.textContent === "") {
+//     refs.spanName.textContent = "Anonymous";
+//   }
+// });
