705c44e40ec99ec00c083c1a192e470cdeecb213
GannaKov <zlatta2000@gmail.com>
2022-09-13 01:10:11 +0300

    Task 10 (create div)

--- src/src/js/task-10.js
+++ src/src/js/task-10.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const refs = {
+  buttonCreate: document.querySelector("button[data-create]"),
+  buttonDestroy: document.querySelector("button [data-destroy]"),
+  divBoxesEl: document.querySelector("#boxes"),
+  inputEl: document.querySelector("#controls>input"),
+};
+refs.buttonCreate.addEventListener("click", onbuttonCreateClick);
+function onbuttonCreateClick(event) {
+  let divElements = [];
+  const divAmount = refs.inputEl.value;
+  for (let i = 1; i <= divAmount; i++) {
+    divElements.push(document.createElement("div"));
+  }
+  refs.divBoxesEl.append(...divElements);
+}
