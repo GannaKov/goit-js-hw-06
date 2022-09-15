2f3c97b50264afc3a278dbaf64dd5add9abb6ff3
GannaKov <zlatta2000@gmail.com>
2022-09-15 20:47:24 +0300

    Task 1 upd

--- src/src/js/task-01.js
+++ src/src/js/task-01.js
@@ -1,18 +1,25 @@
-const categoriesEl = document.querySelector("#categories");
-
-const categoriesListEl = categoriesEl.querySelectorAll("ul");
-
+const categoriesListEl = document.querySelectorAll(".item");
 console.log(`Number of categories:${categoriesListEl.length}`);
 
-const categoriesItemTitleArr = categoriesEl.querySelectorAll("h2");
-for (let i = 0; i < categoriesListEl.length; i++) {
-  const categoriesItemTitle = categoriesItemTitleArr[i].textContent;
+categoriesListEl.forEach((elem) => {
+  console.log("Category:", elem.querySelector("h2").textContent);
+  console.log("Elements:", elem.querySelectorAll("li").length);
+});
+// --------------- Var --------------
+// const categoriesEl = document.querySelector("#categories");
 
-  const categoriesQuantityEl =
-    categoriesListEl[i].querySelectorAll("li").length;
-  console.log(`Category:${categoriesItemTitle}`);
-  console.log(`Elements:${categoriesQuantityEl}`);
-}
+// const categoriesListEl = categoriesEl.querySelectorAll("ul");
+
+// console.log(`Number of categories:${categoriesListEl.length}`);
+// const categoriesItemTitleArr = categoriesEl.querySelectorAll("h2");
+// for (let i = 0; i < categoriesListEl.length; i++) {
+//   const categoriesItemTitle = categoriesItemTitleArr[i].textContent;
+
+//   const categoriesQuantityEl =
+//     categoriesListEl[i].querySelectorAll("li").length;
+//   console.log(`Category:${categoriesItemTitle}`);
+//   console.log(`Elements:${categoriesQuantityEl}`);
+// }
 // ---------------Var2 запасной--------------
 // const categoriesListEl1 = categoriesEl.children; //масс детей главной ul
 
--- src/src/js/task-10.js
+++ src/src/js/task-10.js
@@ -1,3 +1,4 @@
+// поробовать все удалить innerHTML=""
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
